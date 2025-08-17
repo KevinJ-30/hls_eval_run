899649e9fde667f6702fc43fcd736789f798755c
unknown <kevinmatjacob@gmail.com>
2025-08-17 11:29:30 -0400

    minor bug fixes

--- hls_eval_experiments/test_zero_shot_gen/exp.py
+++ hls_eval_experiments/test_zero_shot_gen/exp.py
@@ -24,7 +24,7 @@ LOGGER = logging.getLogger(EXP_NAME)
 LOGGER.propagate = True
 LOGGER.setLevel(logging.DEBUG)
 
-API_KEY_OPENROUTER = check_key(dotenv_values(".env")["OPENROUTER_API_KEY"])
+API_KEY_OPENROUTER = check_key(dotenv_values(".env")["OPEN_ROUTER_API_KEY"])
 
 
 if __name__ == "__main__":
@@ -43,8 +43,8 @@ if __name__ == "__main__":
     all_benchmark_cases_map = {bc.name: bc for bc in all_benchmark_cases}
 
     model_names_to_test = [
-        "openai/gpt-4o-mini",
-        "meta-llama/llama-3.1-8b-instruct",
+        "openai/gpt-oss-20b:free",
+        "qwen/qwen-2.5-coder-32b-instruct:free"
     ]
     models = [
         build_model_remote_openrouter(model_name, api_key=API_KEY_OPENROUTER)
@@ -83,7 +83,7 @@ if __name__ == "__main__":
         # ("heat-3d", "meta-llama/Llama-3-8b-chat-hf"),
         # ("mix_columns", "google/gemma-2-27b-it"),
         # ("mix_columns", "meta-llama/Llama-3-70b-chat-hf"),
-        ("des", "openai/gpt-4o-mini"),
+        ("des", "openai/gpt-oss-20b:free"),
         # ("monobit", "Qwen/Qwen2.5-Coder-32B-Instruct"),
         # ("monobit", "google/gemma-2-27b-it"),
         # ("monobit", "meta-llama/Llama-3-70b-chat-hf"),
